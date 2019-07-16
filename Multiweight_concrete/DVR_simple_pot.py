Trying to find the bug in the descendant weighting part of multi-weight

Multiweight_concrete/DVR_simple_pot.py
@@ -62,7 +62,7 @@ def run(Ecut):
     return En, Eig, g
 
 
-cut = 10
+cut = 100
 En0, Eig0, g = run(0)
 En10, Eig10, g10 = run(cut)
 overlap = np.linalg.norm(Eig0[:, 0]*Eig10[:, 0], ord=1)

Multiweight_concrete/Simple_pot_multiweight.py
@@ -1,5 +1,7 @@
 import numpy as np
 import copy
+#import Timing_p3 as tm
+import matplotlib.pyplot as plt
 
 # DMC parameters
 dtau = 5.
@@ -61,61 +63,37 @@ def V_ref_calc(Psi):
 
 
 def Weighting(Vref, Psi, DW):
-    global walker_reaper
     cuts = len(Vref)
     for i in range(cuts):
         Psi.weights[i, :] *= np.exp(-(Psi.V[i, :] - Vref[i]) * dtau)
     threshold = 0.01
-    # method 1
-    # for j in range(cuts):
-    #     death = np.argwhere(Psi.weights[j, :] < threshold)
-    #     for i in death:
-    #         ind = np.unravel_index(Psi.weights.argmax(), Psi.weights.shape)
-    #         if DW is True:
-    #             Biggo_num = float(Psi.walkers[ind[1]])
-    #             Psi.walkers[i] = Biggo_num
-    #         Biggo_weight = np.array(Psi.weights[:, ind[1]])
-    #         Biggo_pos = float(Psi.coords[ind[1]])
-    #         Biggo_pot = np.array(Psi.V[:, ind[1]])
-    #         Biggo_diff = np.array(Psi.Diff[:, ind[1]])
-    #         Psi.coords[i[0]] = Biggo_pos
-    #         Psi.weights[:, i[0]] = Biggo_weight/2.
-    #         Psi.weights[:, ind[1]] = Biggo_weight/2.
-    #         Psi.V[:, i[0]] = Biggo_pot
-    #         Psi.Diff[:, i[0]] = Biggo_diff
-
-    # method 2
-    death = np.argwhere(Psi.weights < threshold)
-    j = 0
+    death = np.argwhere(np.all(Psi.weights < threshold, axis=0))
     for i in death:
-        if Psi.weights[i[0], i[1]] == np.max(Psi.weights[:, i[1]]):
-            j += 1
-            ind = np.unravel_index(Psi.weights.argmax(), Psi.weights.shape)
-            if DW is True:
-                Biggo_num = float(Psi.walkers[ind[1]])
-                Psi.walkers[i] = Biggo_num
-            Biggo_weight = np.array(Psi.weights[:, ind[1]])
-            Biggo_pos = float(Psi.coords[ind[1]])
-            Biggo_pot = np.array(Psi.V[:, ind[1]])
-            Biggo_diff = np.array(Psi.Diff[:, ind[1]])
-            Psi.coords[i[1]] = Biggo_pos
-            Psi.weights[:, i[1]] = Biggo_weight / 2.
-            Psi.weights[:, ind[1]] = Biggo_weight / 2.
-            Psi.V[:, i[1]] = Biggo_pot
-            Psi.Diff[:, i[1]] = Biggo_diff
-    walker_reaper = np.append(walker_reaper, j)
+        ind = np.unravel_index(Psi.weights.argmax(), Psi.weights.shape)
+        if DW is True:
+            Biggo_num = float(Psi.walkers[ind[1]])
+            Psi.walkers[i[0]] = Biggo_num
+        Biggo_weight = np.array(Psi.weights[:, ind[1]])
+        Biggo_pos = float(Psi.coords[ind[1]])
+        Biggo_pot = np.array(Psi.V[:, ind[1]])
+        Biggo_diff = np.array(Psi.Diff[:, ind[1]])
+        Psi.coords[i[0]] = Biggo_pos
+        Psi.weights[:, i[0]] = Biggo_weight / 2.
+        Psi.weights[:, ind[1]] = Biggo_weight / 2.
+        Psi.V[:, i[0]] = Biggo_pot
+        Psi.Diff[:, i[0]] = Biggo_diff
+
     return Psi
 
 
 def descendants(Psi):
     d = np.zeros((len(Psi.weights[:, 0]), N_0))
     for i in range(N_0):
-        d[:, i] = np.sum(Psi.weights[:, Psi.walkers == i])
+        d[:, i] = np.sum(Psi.weights[:, Psi.walkers == i], axis=1)
     return d
 
 
 def run(equilibration, wait_time, propagation, Ecut, naming):
-    global walker_reaper
     barrier = 1000.
     spacing = 2.
     cuts = len(Ecut)
@@ -180,21 +158,41 @@ def run(equilibration, wait_time, propagation, Ecut, naming):
     np.save("DMC_HO_Psi_pos_concrete_multiweight%s" %naming, positions)
     np.save("DMC_HO_Psi_weights_concrete_multiweight%s" %naming, weights)
 
-    return
+    return des_weights, differences, Eref, positions, weights
 
 
-def acquire_dis_data():
-    for i in range(1):
-        for j in range(1):
-            Ecut_array = np.linspace(0, 10*(i+1), num=(j+2))
-            run(4000, 500, 100, Ecut_array, '_to_%s_job' %Ecut_array[-1] + '_%s' %(j+1))
-            print('Done with Ecut to %s' %Ecut_array[-1] + ' job %s' %(j+1))
-
-
-acquire_dis_data()
+# def acquire_dis_data():
+#     for i in range(5):
+#         for j in range(5):
+#             Ecut_array = np.linspace(0, 100*(i+1), num=(5))
+#             run(4000, 500, 10*(j+1), Ecut_array, '_to_%s_des' %Ecut_array[-1] + '_%s' %(10 * (j+1)))
+#             print('Done with Ecut to %s' %Ecut_array[-1] + ' des %s!' %(10*(j+1)))
+#
+#
+# acquire_dis_data()
 
 # Ecut_array = np.zeros(2)
-# run(4000, 500, 50, Ecut_array, '_test_with_no_cut')
+# DW, time_list = tm.time_me(run, 4000, 500, 50, Ecut_array, '_test_with_no_cut')
+# tm.print_time_list(run, time_list)
+Ecut_array = np.linspace(0, 100, num=5)
+d, diff, erf, pos, weights = run(4000, 500, 50, Ecut_array, 'testing_death')
+
+dw = len(d[:, 0, 0])
+cuts = len(d[0, :, 0])
+
+for i in range(dw):
+    for j in range(cuts):
+        amp, xx = np.histogram(pos[i, :], weights=d[i, j, :], bins=25, range=(-1.75, 1.75), density=True)
+        bins = (xx[1:] + xx[:-1])/2.
+        plt.plot(bins, amp, label='Des weight {0} Ecut {1}'.format((i+1), Ecut_array[j]))
+        plt.ylabel('Probability Amplitude')
+        plt.legend()
+        plt.ylim(0, 0.7)
+        plt.savefig('Psisqrd_des_weight_{0}_Ecut_{1}.png'.format((i+1), Ecut_array[j]))
+        plt.close()
+
+
+
 
 
 

Multiweight_concrete/Simple_pot_workup.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 har2wave = 219474.6
-exact_value = [452.984883748748, 452.9857207971296]#453.58024614005933, 456.8115157826138, 464.03340693238886, 476.2524479756547, 494.31143851304546]
+exact_value = [452.984883748748, 453.58024614005933, 456.8115157826138, 464.03340693238886, 476.2524479756547, 494.31143851304546]
 DVR_no_correct = [452.9848837487481, 479.86017397722145, 525.5312334774873, 580.1326581308361, 639.8628850982727, 702.5739445389472]
 
 
@@ -10,6 +10,7 @@ def load_psi(which_one):
     Psi_coords = np.load("DMC_HO_Psi_pos_concrete_multiweight%s.npy" %which_one)
     Psi_d = np.load("DMC_HO_descendants_concrete_multiweight%s.npy" %which_one)
     Psi_weights = np.load("DMC_HO_Psi_weights_concrete_multiweight%s.npy" %which_one)
+    # Psi_d = np.zeros(Psi_weights.shape) + 1.
     return Psi_coords, Psi_d, Psi_weights
 
 
@@ -63,27 +64,29 @@ def lets_get_these_graphs(name, Ecut_array):
     #     Energies_nm[j] += np.mean(energy[0, 500:]) + E_correct_mean_nm[j]
     # # plt.errorbar(Ecut, Energies_nm*har2wave, yerr=(E_correct_std_nm*har2wave), label='Non_Multi_weight')
     # plt.plot(Ecut, Energies_nm*har2wave, label='Non_Multi_weight')
-    for i in range(1):
+    for i in range(5):
         fig, axes = plt.subplots()
-        for l in range(1):
+        for l in range(5):
             coords, d, weights = load_psi(name[i][l])
             Psi = Walkers(coords, d, weights)
             diffs, energy = load_observables(name[i][l])
             num_dw = len(Psi.d[:, 0, 0])
             cuts = len(Psi.d[0, :, 0])
-            Ecut = np.linspace(0, Ecut_array[i, l], num=(l+2))
+            Ecut = np.linspace(0, Ecut_array[i, l], num=(5))
             E_corrections = np.zeros((num_dw, cuts))
             E_correct_mean = np.zeros(cuts)
             E_correct_std = np.zeros(cuts)
             Energies = np.zeros(cuts)
+            if np.sum(Psi.d[-1, 0, :]) == 0.:
+                print("that's no good")
             for j in range(cuts):
                 for k in range(num_dw):
                     E_corrections[k, j] += sum(Psi.d[k, j, :] * diffs[k, j, :]) / sum(Psi.d[k, j, :])
                 E_correct_mean[j] += np.mean(E_corrections[:, j])
                 E_correct_std[j] += np.std(E_corrections[:, j])
                 Energies[j] += np.mean(energy[j, 500:]) + E_correct_mean[j]
-            axes.plot(Ecut, Energies * har2wave, label='Num of points %s' %(l+2))
-        x = np.linspace(0, 10*(i+1), num=(i+2))
+            axes.plot(Ecut, Energies * har2wave, label='Num of Des Weight timesteps %s' %(10*(l+1)))
+        x = np.linspace(0, 100*(i+1), num=(i+2))
         DVR = np.array([])
         for j in range(i+2):
             DVR = np.append(DVR, exact_value[j])
@@ -91,9 +94,9 @@ def lets_get_these_graphs(name, Ecut_array):
         axes.set_xlabel('Ecut (cm^-1)')
         axes.set_ylabel('Ground State Energy (cm^-1)')
         axes.set_title('Ground State Energy with Different Flattening')
-        axes.set_ylim(452, 454)
-        lgd = axes.legend(loc='center right', bbox_to_anchor=(1.37, 0.5))
-        fig.savefig('Energy_after_flattening_multi_simple_more_walkers_other_method%s.png' %Ecut_array[i, 0], bbox_extra_artists=(lgd,), bbox_inches='tight')
+        # axes.set_ylim(450, 520)
+        lgd = axes.legend(loc='center right', bbox_to_anchor=(1.67, 0.5))
+        fig.savefig('Energy_after_flattening_multi_simple_more_walkers%s.png' %(str(Ecut_array[i, 0]) + '_des_test'), bbox_extra_artists=(lgd,), bbox_inches='tight')
         plt.close(fig)
 
 
@@ -111,17 +114,17 @@ def lets_get_these_graphs(name, Ecut_array):
 
 
 # Psi_sqrd(7, 25)
-# names = [['']*5, ['']*5, ['']*5, ['']*5, ['']*5]
-# Ecuts = np.zeros((5, 5))
-# for i in range(5):
-#     Ecuts[i, :] += 100*(i+1)
-#     for j in range(5):
-#         names[i][j] += '_to_%s_job' % Ecuts[i, j] + '_{0}'.format(j+1)
-# lets_get_these_graphs(names, Ecuts)
+names = [['']*5, ['']*5, ['']*5, ['']*5, ['']*5]
+Ecuts = np.zeros((5, 5))
+for i in range(5):
+    Ecuts[i, :] += 100*(i+1)
+    for j in range(5):
+        names[i][j] += '_to_%s_des' % Ecuts[i, j] + '_{0}'.format(10*(j+1))
+lets_get_these_graphs(names, Ecuts)
 
-name = [['_to_10.0_job_1']]
-Ecuts = np.array([[10.]])
-lets_get_these_graphs(name, Ecuts)
+# name = [['_to_10.0_job_1']]
+# Ecuts = np.array([[10.]])
+# lets_get_these_graphs(name, Ecuts)
 
 
 

Overlaps/DVR_OH_stretches.py
@@ -68,7 +68,7 @@ def run(file_name, num, mass):
 mass1 = (m_O*m_H)/(m_O+m_H)
 mass2 = ((m_O+m_H)*m_O)/(2.*m_O+m_H)
 x = run('mono-roh-pot.dat', 'monomer', mass1)
-run('trimer-rohb-pot.dat', 'trimer', mass1)
+run('trimer-rohb-pot.dat', 'trimer', mass2)
 
 mono_gsw = np.load('monomer_GSW.npy')
 tri_gsw = np.load('trimer_GSW.npy')