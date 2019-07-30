97ff53a95aefe1251b78bb67ffa93f68606090b1
finneyjm <finneyjm@uw.edu>
2019-07-30 15:26:27 -0700

    Have a rough draft of the importance sampling code for CH5

--- Imp_samp_testing/CH5_imp_samp.py
+++ Imp_samp_testing/CH5_imp_samp.py
@@ -1,13 +1,13 @@
-import numpy as np
 import copy
 import CH5pot
 from scipy import interpolate
 from Coordinerds.CoordinateSystems import *
+import Timing_p3 as tm
 
 # DMC parameters
 dtau = 1.
-N_0 = 500
-time_steps = 1000.
+N_0 = 10000
+time_steps = 1.
 alpha = 1./(2.*dtau)
 
 # constants and conversion factors
@@ -22,6 +22,7 @@ har2wave = 219474.6
 sigmaH = np.sqrt(dtau/m_H)
 sigmaC = np.sqrt(dtau/m_C)
 sigmaCH = np.sqrt(dtau/m_CH)
+bonds = 5
 # Starting orientation of walkers
 coords_initial = np.array([[0.000000000000000, 0.000000000000000, 0.000000000000000],
                           [0.1318851447521099, 2.088940054609643, 0.000000000000000],
@@ -31,7 +32,7 @@ coords_initial = np.array([[0.000000000000000, 0.000000000000000, 0.000000000000
                           [-0.8247121421923925, -0.6295306113384560, 1.775332267901544]])
 order = [[0, 0, 0, 0], [1, 0, 0, 0], [2, 0, 1, 0], [3, 0, 1, 2], [4, 0, 1, 2], [5, 0, 1, 2]]
 
-Psi_t = np.load('Average_GSW_CH_stretch.npy')
+Psi_t = np.load('Switch_min_wvfn_speed_1.0.npy')
 interp = interpolate.splrep(Psi_t[0, :], Psi_t[1, :], s=0)
 
 
@@ -50,36 +51,35 @@ class Walkers(object):
 
 
 def psi_t(zmatrix):
-    Psi_t = np.zeros((len(zmatrix[:, 1]), N_0))
-    for i in range(len(zmatrix[:, 1])):
-        Psi_t[i, :] += interpolate.splev(zmatrix[i, 1], interp, der=0)
-    return Psi_t
+    psi = np.zeros((N_0, bonds))
+    for i in range(bonds):
+        psi[:, i] += interpolate.splev(zmatrix[:, i, 1], interp, der=0)
+    return psi
 
 
 def drift(zmatrix):
     psi = psi_t(zmatrix)
-    der = np.zeros((len(zmatrix[:, 1]), N_0))
-    for i in range(len(zmatrix[:, 1])):
-        der[i, :] += interpolate.splev(zmatrix[i, 1], interp, der=1)
+    der = np.zeros((N_0, bonds))
+    for i in range(bonds):
+        der[:, i] += interpolate.splev(zmatrix[:, i, 1], interp, der=1)
     return der/psi
 
 
 def metropolis(x, y, Fqx, Fqy):
     psi_x = psi_t(x)
     psi_y = psi_t(y)
-    dof = len(x[:, 1])
-    a = np.zeros(dof) + 1.
-    for i in range(dof):
-        a *= (psi_y[i, :]/psi_x[i, :])**2 * np.exp(1./2.*(Fqx + Fqy)*(sigmaH**2/4.*(Fqx-Fqy) - (y[i, 1]-x[i, 1])))
+    a = 1.
+    for i in range(bonds):
+        a *= (psi_y[:, i]/psi_x[:, i])**2 * np.exp(1./2.*(Fqx[:, i] + Fqy[:, i])*(sigmaCH**2/4.*(Fqx[:, i]-Fqy[:, i])
+                                                                                  - (y[:, i, 1]-x[:, i, 1])))
     return a
 
 
 # Random walk of all the walkers
 def Kinetic(Psi, Fqx):
-    # randomwalkC = np.random.normal(0.0, sigmaC, size=(N_0, 3))
     zmatrix = CoordinateSet(Psi.coords, system=CartesianCoordinates3D).convert(ZMatrixCoordinates, ordering=order).coords
     Drift = sigmaCH**2/2.*Fqx
-    zmatrix[:, 1] += Drift
+    zmatrix[:, :, 1] += Drift
     y = CoordinateSet(zmatrix, system=ZMatrixCoordinates).convert(CartesianCoordinates3D).coords
     randomwalkH = np.zeros((N_0, 6, 3))
     randomwalkH[:, 1:6, :] = np.random.normal(0.0, sigmaH, size=(N_0, 5, 3))
@@ -92,7 +92,6 @@ def Kinetic(Psi, Fqx):
     Psi.coords[accept] = y[accept]
     nah = np.argwhere(a <= check)
     Fqy[nah] = Fqx[nah]
-    # Psi.coords[:, 0, :] += randomwalkC
     return Psi, Fqy
 
 
@@ -102,15 +101,61 @@ def Potential(Psi):
     return Psi
 
 
-def sec_dir(coords):
-    return interpolate.splev(coords, interp, der=2)
+def drdx(coords, zmatrix):
+    chain = np.zeros((N_0, 2, 3, 5))
+    for j in range(3):
+        for l in range(5):
+            chain[:, 0, j, l] -= coords[:, 0, j]/zmatrix[:, l, 1]
+            chain[:, 1, j, l] += coords[:, 0, j]/zmatrix[:, l, 1]
+    return chain
+
+
+def drdx2(coords, zmatrix):
+    chain = np.zeros((N_0, 6, 3, 5))
+    for j in range(3):
+        for l in range(5):
+            chain[:, 0, j, l] += 1./zmatrix[:, l, 1] - coords[:, 0, j]**2/zmatrix[:, l, 1]**3
+            chain[:, 1, j, l] += 1./zmatrix[:, l, 1] - coords[:, 0, j]**2/zmatrix[:, l, 1]**3
+    return chain
+
+
+def local_kinetic(coords):
+    zmatrix = CoordinateSet(coords, system=CartesianCoordinates3D).convert(ZMatrixCoordinates, ordering=order).coords
+    psi = psi_t(zmatrix)
+    psi_new = 1.
+    for i in range(bonds):
+        psi_new *= psi[:, i]
+    dr1 = drdx(coords, zmatrix)
+    dr2 = drdx2(coords, zmatrix)
+
+    der1 = np.zeros((N_0, bonds))
+    for i in range(bonds):
+        der1[:, i] += interpolate.splev(zmatrix[:, i, 1], interp, der=1)
+
+    der2 = np.zeros((N_0, bonds))
+    for i in range(bonds):
+        der2[:, i] += interpolate.splev(zmatrix[:, i, 1], interp, der=2)
+
+    kin = 0.
+    for i in range(6):
+        if i == 0:
+            mass = sigmaC
+            for j in range(3):
+                stretch_kin = 1./mass
+                for l in range(5):
+                    stretch_kin *= (der2[:, l] * dr1[:, 0, j, l]**2) + (der1[:, l] * dr2[:, 0, j, l])
+                kin += stretch_kin
+        else:
+            mass = sigmaH
+            for j in range(3):
+                stretch_kin = 1./mass * (der2[:, i-1] * dr1[:, 1, j, i-1]**2) + (der1[:, i-1] * dr2[:, 1, j, i-1])
+                kin += stretch_kin
+
+    return (-1./2.*kin)/psi_new
 
 
 def E_loc(Psi):
-    psi = psi_t(Psi.coords)
-    kin = -1./(2.*m_CH)*sec_dir(Psi.coords)/psi
-    pot = Psi.V
-    Psi.El = kin + pot
+    Psi.El = local_kinetic(Psi.coords) + Psi.V
     return Psi
 
 
@@ -180,9 +225,9 @@ def run(propagation):
             DW = True
         elif i >= (time_steps - 1. - float(propagation)) and prop == 0.:
             d_values = descendants(new_psi)
-            Psi_tau.d += d_values
+    return DW
 
 
-
-
+dw, time = tm.time_me(run, 0)
+tm.print_time_list(run, time)
 
