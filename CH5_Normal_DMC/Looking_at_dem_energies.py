Fixed a slight bug in my descendant weighting calcs

CH5_Normal_DMC/Looking_at_dem_energies.py
@@ -24,32 +24,33 @@ def lets_get_some_energies(non_imp_samp_walkers, imp_samp_walkers, trials_ni, tr
             #                  f'Walkers_Test_{j+1}.npz')['Eref']*har2wave
             # energies_non[i, j] += np.mean(Energy[5000:])
             #
-            Energy = np.load(f'Trial_wvfn_testing/results/Non_imp_sampled_pdimer/' +
-                              f'Non_imp_sampled_pdimer_{non_imp_samp_walkers[i]}_' +
-                              f'Walkers_Test_{j+1}.npz')['Eref']*har2wave
-            energies_non[i, j] += np.mean(Energy[5000:]) + 11792.2255946
-            # energies_non[i,j] += np.mean(Energy[5000:]) +(9.12996e-2*har2wave)
+            # Energy = np.load(f'Trial_wvfn_testing/results/Non_imp_sampled_pdimer/' +
+            #                   f'Non_imp_sampled_pdimer_{non_imp_samp_walkers[i]}_' +
+            #                   f'Walkers_Test_{j+1}.npz')['Eref']*har2wave
+            # energies_non[i, j] += np.mean(Energy[5000:]) + 11792.2255946
+            # energies_non[i,j] += np.mean(Energy[5000:]) + (9.129961343400107E-002*har2wave)
 
             # Energy = np.load(f'Trial_wvfn_testing/results/ptrimer_non_imp_samp/' +
-            #                  f'ptrimer_non_imp_samp_{imp_samp_walkers[i]}_' +
+            #                  f'ptrimer_non_imp_samp_{non_imp_samp_walkers[i]}_' +
             #                  f'Walkers_Test_{j+1}.npz')['Eref'] * har2wave
             # a = -9.129961343400107E-002 * har2wave
             # energies_non[i, j] = np.mean(Energy[5000:]) - a
 
 
-            #Energy = np.load(f'Trial_wvfn_testing/results/ptetramer_non_imp_samp/' +
-            #                 f'ptetramer_non_imp_samp_{imp_samp_walkers[i]}_' +
-            #                 f'Walkers_Test_{j+1}.npz')['Eref'] * har2wave
-            #a = -0.122146858971399 * har2wave
-            #energies_non[i, j] = np.mean(Energy[5000:]) - a
+            Energy = np.load(f'Trial_wvfn_testing/results/ptetramer_non_imp_samp/' +
+                            f'ptetramer_non_imp_samp_{non_imp_samp_walkers[i]}_' +
+                            f'Walkers_Test_{j+1}.npz')['Eref'] * har2wave
+            a = -0.122146858971399 * har2wave
+            energies_non[i, j] = np.mean(Energy[5000:]) - a
 
             # Energy = np.load(f'Trial_wvfn_testing/results/pmonomer_non_imp_samp/' +
-            #                  f'pmonomer_non_imp_samp_{imp_samp_walkers[i]}_' +
+            #                  f'pmonomer_non_imp_samp_{non_imp_samp_walkers[i]}_' +
             #                  f'Walkers_Test_{j + 1}.npz')['Eref'] * har2wave
             # a = 3.404489490321794E-006 * har2wave
             # a = 0.00395373*har2wave
             # a = 0.00500124*har2wave
             # a = -9.129961343400107E-002 * har2wave
+            # a = 0*har2wave
             # energies_non[i, j] = np.mean(Energy[5000:]) - a
 
     for j in range(trials_i):
@@ -63,25 +64,30 @@ def lets_get_some_energies(non_imp_samp_walkers, imp_samp_walkers, trials_ni, tr
             #                  f'Walkers_Test_{j+1}.npz')['Eref']*har2wave
             # energies_imp[i, j] += np.mean(Energy[5000:])
 
-            Energy = np.load(f'Trial_wvfn_testing/results/pdimer_waters_described/' +
-                              f'pdimer_waters_described_{non_imp_samp_walkers[i]}_' +
-                              f'Walkers_Test_{j + 1}.npz')['Eref'] * har2wave
-            energies_imp[i, j] = np.mean(Energy[5000:]) + 11792.22559467
-            # energies_imp[i, j] = np.mean(Energy[5000:]) + (9.12996e-2*har2wave)
+            # Energy = np.load(f'Trial_wvfn_testing/results/pdimer_waters_described/' +
+            #                   f'pdimer_waters_described_{non_imp_samp_walkers[i]}_' +
+            #                   f'Walkers_Test_{j + 1}.npz')['Eref'] * har2wave
+            # energies_imp[i, j] = np.mean(Energy[5000:]) + 11792.22559467
+            # energies_imp[i, j] = np.mean(Energy[5000:]) + (9.129961343400107E-002*har2wave)
+
             # Energy = np.load(f'Trial_wvfn_testing/results/ptrimer_full_imp_samp/' +
             #                  f'ptrimer_full_imp_samp_{imp_samp_walkers[i]}_' +
             #                  f'Walkers_Test_{j+1}.npz')['Eref'] * har2wave
             # a = -9.129961343400107E-002 * har2wave
             # energies_imp[i, j] = np.mean(Energy[5000:]) - a
 
-            #Energy = np.load(f'Trial_wvfn_testing/results/ptetramer_full_imp_samp/' +
-            #                 f'ptetramer_full_imp_samp_{imp_samp_walkers[i]}_' +
-            #                 f'Walkers_Test_{j+1}.npz')['Eref'] * har2wave
-            #a = -0.122146858971399 * har2wave
-            #energies_imp[i, j] = np.mean(Energy[5000:]) - a
+            Energy = np.load(f'Trial_wvfn_testing/results/ptetramer_full_imp_samp/' +
+                            f'ptetramer_full_imp_samp_{imp_samp_walkers[i]}_' +
+                            f'Walkers_Test_{j+1}.npz')['Eref'] * har2wave
+            a = -0.122146858971399 * har2wave
+            energies_imp[i, j] = np.mean(Energy[5000:]) - a
+            # Energy = np.load(f'Trial_wvfn_testing/results/ptetramer_w_tetramer_params_imp_samp/' +
+            #                  f'ptetramer_w_tetramer_params_imp_samp_{imp_samp_walkers[i]}_' +
+            #                  f'Walkers_Test_{j+1}.npz')['Eref'] * har2wave
+            # energies_imp[i, j] = np.mean(Energy[5000:]) - a
 
-            # Energy = np.load(f'Trial_wvfn_testing/results/pmonomer_full_imp_samp/' +
-            #                  f'pmonomer_full_imp_samp_{imp_samp_walkers[i]}_' +
+            # Energy = np.load(f'Trial_wvfn_testing/results/pmonomer_imp_samp/' +
+            #                  f'pmonomer_imp_samp_{imp_samp_walkers[i]}_' +
             #                  f'Walkers_Test_{j+1}.npz')['Eref'] * har2wave
             # a = 0 * har2wave
             # energies_imp[i, j] = np.mean(Energy[5000:]) - a
@@ -89,10 +95,12 @@ def lets_get_some_energies(non_imp_samp_walkers, imp_samp_walkers, trials_ni, tr
     avg_imp = np.mean(energies_imp, axis=1)
     print(avg_imp)
     std_imp = np.std(energies_imp, axis=1)
+    # std_imp = np.zeros(std_imp.shape)
     print(std_imp)
     avg_non = np.mean(energies_non, axis=1)
     print(avg_non)
     std_non = np.std(energies_non, axis=1)
+    # std_non = np.zeros(std_non.shape)
     print(std_non)
     fig, axes = plt.subplots(2, 1, sharex=True, gridspec_kw={'hspace': 0})
     axes[0].errorbar(non_imp_samp_walkers, avg_non, yerr=std_non, marker='s', markerfacecolor='none', color='red', label='No Impt. Samp.')
@@ -122,10 +130,12 @@ def lets_get_some_energies(non_imp_samp_walkers, imp_samp_walkers, trials_ni, tr
     # axes[0].set_ylim(8540, 8600)
     # axes[1].set_ylim(18000, 18199)
     # axes[0].set_ylim(18001, 18200)
-    #axes[1].set_ylim(23300, 23799)
-    #axes[0].set_ylim(23301, 23800)
+    # axes[1].set_ylim(23300, 23799)
+    # axes[0].set_ylim(23301, 23800)
     # axes[1].set_ylim(20580, 20649)
     # axes[0].set_ylim(20581, 20650)
+    # axes[0].set_ylim(7421, 7500)
+    # axes[1].set_ylim(7420, 7499)
     axes[0].legend()
     axes[1].legend()
     plt.tight_layout()
@@ -135,13 +145,30 @@ def lets_get_some_energies(non_imp_samp_walkers, imp_samp_walkers, trials_ni, tr
 
 walkers1 = [100, 200, 500, 1000, 2000, 5000, 10000, 20000]
 walkers10 = [100, 200, 500, 1000, 2000, 5000, 10000]
+walkers11 = [100, 200, 500, 1000, 2000, 5000, 15000]
+walkers67 = [100, 200, 500, 1000, 2000, 5000, 20000]
+walkers89 = [1000, 2000, 5000]
 walkers3 = [100, 200, 500, 1000, 2000, 5000, 10000, 15000, 20000, 25000]
 walkers5 = [100, 200, 500, 1000, 2000, 5000, 10000, 15000, 20000, 25000, 60000]
 walkers6 = [100, 200, 500, 1000, 2000, 5000, 10000, 15000, 20000, 25000, 40000, 50000, 60000]
 walkers2 = [500, 1000, 2000, 5000, 10000]
 walkers4 = [100, 200, 500, 1000, 2000, 2500, 3000, 3500, 4000, 4500, 5000,
             5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000, 20000]
-lets_get_some_energies(walkers1, walkers1, 5, 5, 5)
+lets_get_some_energies(walkers89, walkers89, 5, 3, 5)
 plt.show()
 
-
+# blah = np.load(f'Trial_wvfn_testing/results/ptetramer_w_tetramer_params_imp_samp/' +
+#                              f'ptetramer_w_tetramer_params_imp_samp_{5000}_' +
+#                              f'Walkers_Test_{1}.npz')
+# blah2 = np.load(f'Trial_wvfn_testing/results/ptetramer_non_imp_samp/' +
+#                              f'ptetramer_non_imp_samp_{5000}_' +
+#                              f'Walkers_Test_{1}.npz')
+#
+# a = blah['weights']
+# a = -0.122146858971399 * har2wave
+# b = blah['des']
+# eref = blah['Eref']
+# plt.plot(blah2['Eref'])
+# plt.plot(eref)
+# plt.show()
+# plt.close()

lets_go_girls/jobs/Prot_water_funcs/Imp_sample.py
@@ -124,9 +124,12 @@ def psi_t_extra(coords, atoms, num_waters, interp_reg_oh, interp_hbond=None, int
         for i in range(5):
             psi[:, i] = interpolate.splev(reg_oh[:, i], interp_reg_oh, der=0)
         for j in range(2):
-            shift[:, j] = shift_calc(hbond_oo[:, j], interp_OO_shift)
-            scale[:, j] = scale_calc(hbond_oo[:, j], interp_OO_scale)
-            psi[:, j+5] = interpolate.splev(scale[:, j]*(hbond_oh[:, j]-shift[:, j]), interp_hbond, der=0)
+            if interp_hbond is None:
+                psi[:, j+5] = np.ones((len(coords)))
+            else:
+                shift[:, j] = shift_calc(hbond_oo[:, j], interp_OO_shift)
+                scale[:, j] = scale_calc(hbond_oo[:, j], interp_OO_scale)
+                psi[:, j+5] = interpolate.splev(scale[:, j]*(hbond_oh[:, j]-shift[:, j]), interp_hbond, der=0)
         for k in range(2):
             p = k+1
             a = p*2 + p + 1
@@ -135,9 +138,12 @@ def psi_t_extra(coords, atoms, num_waters, interp_reg_oh, interp_hbond=None, int
         for i in range(6):
             psi[:, i] = interpolate.splev(reg_oh[:, i], interp_reg_oh, der=0)
         for j in range(3):
-            shift[:, j] = shift_calc(hbond_oo[:, j], interp_OO_shift)
-            scale[:, j] = scale_calc(hbond_oo[:, j], interp_OO_scale)
-            psi[:, j+6] = interpolate.splev(scale[:, j]*(hbond_oh[:, j]-shift[:, j]), interp_hbond, der=0)
+            if interp_hbond is None:
+                psi[:, j+6] = np.ones((len(coords)))
+            else:
+                shift[:, j] = shift_calc(hbond_oo[:, j], interp_OO_shift)
+                scale[:, j] = scale_calc(hbond_oo[:, j], interp_OO_scale)
+                psi[:, j+6] = interpolate.splev(scale[:, j]*(hbond_oh[:, j]-shift[:, j]), interp_hbond, der=0)
         for k in range(3):
             p = k + 1
             a = p * 2 + p + 1
@@ -365,7 +371,7 @@ def E_ref_calc(Psi, alpha):
 
 # Calculate the weights of the walkers and figure out the birth/death if needed
 def Weighting(Eref, Psi, Fqx, dtau, DW):
-    Psi.weights = Psi.weights * np.exp(-(Psi.El - Eref) * dtau)
+    Psi.weights = Psi.weights * np.nan_to_num(np.exp(-(Psi.El - Eref) * dtau))
     threshold = 1./float(len(Psi.coords))
     death = np.argwhere(Psi.weights < threshold)  # should I kill a walker?
     for i in death:

lets_go_girls/jobs/Prot_water_funcs/Non_imp_sampled.py
@@ -5,8 +5,6 @@ import multiprocessing as mp
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 from ProtWaterPES import Potential
 
-from itertools import repeat
-
 # constants and conversion factors
 me = 9.10938356e-31
 Avo_num = 6.0221367e23
@@ -51,10 +49,11 @@ def V_ref_calc(Psi, dtau):
 # The weighting calculation that gets the weights of each walker in the simulation
 def Weighting(Vref, Psi, DW, dtau):
     N_0 = len(Psi.coords)
-    Psi.weights = Psi.weights * np.exp(-(Psi.V - Vref) * dtau)
+    Psi.weights = Psi.weights * np.nan_to_num(np.exp(-(Psi.V - Vref) * dtau))
     # Conditions to prevent one walker from obtaining all the weight
     threshold = 1./float(N_0)
     death = np.argwhere(Psi.weights < threshold)
+    # print(len(death))
     for i in death:
         ind = np.argmax(Psi.weights)
         if DW is True:
@@ -76,6 +75,7 @@ def descendants(Psi):
     d = np.bincount(Psi.walkers, weights=Psi.weights)
     while len(d) < N_0:
         d = np.append(d, 0.)
+    Psi.walkers = np.arange(0, len(Psi.walkers))
     return d
 
 

lets_go_girls/jobs/Prot_water_params/Create_params_files_pwater.py
@@ -1,34 +1,67 @@
-walkers = [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 30000, 40000]
+walkers = [10, 100, 200, 500, 1000, 2000, 5000, 10000, 15000, 20000, 25000, 30000, 40000]
 # walkers = [6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500]
 # walkers = [50000]
 # bro_str = ['5', '10']
 # bro = [5, 10]
 atoms = ['H', 'H', 'H', 'O', 'H', 'H', 'O', 'H', 'H', 'O', 'H', 'H', 'O']
-ts = 10
+ts = 1
+system = 'ptetramer'
+type_of_sim = 'non_imp_samp'
+imp_samp = False
+params = 'tetramer'
 # for j in range(len(bro)):
 # for ts in bro:
 # for j in range(6):
 for i in range(5):
     for x in range(len(walkers)):
-        with open(f'../params/params_ptetramer_non_imp_samp_ts_{ts}_{walkers[x]}_walkers_test_{i+1}.py', 'w') as myfile:
-            myfile.write('import numpy as np\n\n')
-            # myfile.write('from scipy import interpolate\n')
+        with open(f'../params/params_{system}_{type_of_sim}_{walkers[x]}_walkers_test_{i+1}.py', 'w') as myfile:
+            myfile.write('import numpy as np\n')
+            myfile.write('from scipy import interpolate\n')
             # for hs in range(j):
             #     atoms[hs+1] = 'H'
-            myfile.write(f'atoms = {atoms}\n')
-            # myfile.write('wvfn = np.load("params/wvfns/free_oh_wvfn.npy")\n')
-            # myfile.write('hbond_wvfn = np.load("Prot_water_params/wvfns/shared_prot_moveable_wvfn.npy")\n')
-            # myfile.write('free_oh_wvfn = interpolate.splrep(wvfn[:, 0], wvfn[:, 1], s=0)\n')
-            # myfile.write('hbond_wvfn = interpolate.splrep(hbond_wvfn[:, 0], hbond_wvfn[:, 1], s=0)\n')
-            # myfile.write('trial_wvfn = {\n')
-            # myfile.write('    "reg_oh": free_oh_wvfn,\n')
-            # myfile.write('    "ang": None, \n')
-            # myfile.write('    "hbond": hbond_wvfn,\n')
-            # myfile.write('    "OO_shift": np.array(np.loadtxt("Prot_water_params/shared_prot_params/'
-            #              'bowman_h7o3_Re_Polynomials")),\n')
-            # myfile.write('    "OO_scale": np.array(np.loadtxt("Prot_water_params/shared_prot_params/'
-            #              'bowman_h7o3_Std_Polynomials"))\n')
-            # myfile.write('}\n\n')
+            myfile.write(f'atoms = {atoms}\n\n')
+            if imp_samp:
+                myfile.write('wvfn = np.load("params/wvfns/free_oh_wvfn.npy")\n')
+                myfile.write('free_oh_wvfn = interpolate.splrep(wvfn[:, 0], wvfn[:, 1], s=0)\n\n')
+                if system == 'pmonomer':
+                    myfile.write('trial_wvfn = {\n')
+                    myfile.write('    "reg_oh": free_oh_wvfn,\n')
+                    myfile.write('    "ang": None, \n')
+                    myfile.write('}\n\n')
+                else:
+                    if params == 'waters':
+                        myfile.write('trial_wvfn = {\n')
+                        myfile.write('    "reg_oh": free_oh_wvfn,\n')
+                        myfile.write('    "ang": None, \n')
+                        myfile.write('    "hbond": None,\n')
+                        myfile.write('    "OO_shift": None, \n')
+                        myfile.write('    "OO_scale": None, \n')
+                        myfile.write('}\n\n')
+                    elif params == 'tetramer':
+                        myfile.write('hbond_wvfn = np.load("Prot_water_params/wvfns/shared_prot_moveable_wvfn.npy")\n')
+                        myfile.write('hbond_wvfn = interpolate.splrep(hbond_wvfn[:, 0], hbond_wvfn[:, 1], s=0)\n\n')
+                        myfile.write('trial_wvfn = {\n')
+                        myfile.write('    "reg_oh": free_oh_wvfn,\n')
+                        myfile.write('    "ang": None, \n')
+                        myfile.write('    "hbond": hbond_wvfn,\n')
+                        myfile.write('    "OO_shift": np.array(np.loadtxt("Prot_water_params/shared_prot_params/'
+                                     'bowman_h9o4_Re_Polynomials")),\n')
+                        myfile.write('    "OO_scale": np.array(np.loadtxt("Prot_water_params/shared_prot_params/'
+                                     'bowman_h9o4_Std_Polynomials"))\n')
+                        myfile.write('}\n\n')
+                    elif params == 'trimer':
+                        myfile.write('hbond_wvfn = np.load("Prot_water_params/wvfns/shared_prot_moveable_wvfn.npy")\n')
+                        myfile.write('hbond_wvfn = interpolate.splrep(hbond_wvfn[:, 0], hbond_wvfn[:, 1], s=0)\n\n')
+                        myfile.write('trial_wvfn = {\n')
+                        myfile.write('    "reg_oh": free_oh_wvfn,\n')
+                        myfile.write('    "ang": None, \n')
+                        myfile.write('    "hbond": hbond_wvfn,\n')
+                        myfile.write('    "OO_shift": np.array(np.loadtxt("Prot_water_params/shared_prot_params/'
+                                     'bowman_h7o3_Re_Polynomials")),\n')
+                        myfile.write('    "OO_scale": np.array(np.loadtxt("Prot_water_params/shared_prot_params/'
+                                     'bowman_h7o3_Std_Polynomials"))\n')
+                        myfile.write('}\n\n')
+
             # myfile.write(f'wvfn_D = np.load("params/min_wvfns/GSW_min_CD_2.npy")\n')
             # myfile.write(f'wvfn_H = np.load("params/min_wvfns/GSW_min_CH_2.npy")\n\n')
             # myfile.write('wvfn = np.zeros((5, 5000))\n')
@@ -44,19 +77,20 @@ for i in range(5):
             # myfile.write(f'    wvfn[CH] = np.vstack((x, np.load(f"params/min_wvfns/GSW_min_CH_{{CH+1}}.npy")))\n\n')
             myfile.write('pars = {\n')
             myfile.write(f'    "N_0": {walkers[x]},\n')
-            myfile.write('    "system": "ptetramer",\n')
+            myfile.write(f'    "system": "{system}",\n')
             myfile.write('    "time_steps": 20000,\n')
             myfile.write(f'    "dtau": {ts},\n')
             myfile.write('    "multicore": True,\n')
             myfile.write('    "equilibration": 5000,\n')
             myfile.write('    "wait_time": 500,\n')
-            myfile.write(f'    "output": "ptetramer_non_imp_samp_ts_{ts}_{walkers[x]}_Walkers_Test_{i+1}",\n')
+            myfile.write(f'    "output": "{system}_{type_of_sim}_{walkers[x]}_Walkers_Test_{i+1}",\n')
             myfile.write('    "atoms": atoms,\n')
-            # myfile.write('    "imp_samp": True,\n')
-            # myfile.write('    "trial_wvfn": trial_wvfn,\n')
+            if imp_samp:
+                myfile.write('    "imp_samp": True,\n')
+                myfile.write('    "trial_wvfn": trial_wvfn,\n')
             # myfile.write('    "imp_samp_type": "dev_dep",\n')
             # myfile.write('    "hh_relate": hh,\n')
             # myfile.write('    "rand_samp": False,\n')
             myfile.write('}\n\n')
-            myfile.write(f'output_dir = "ptetramer_non_imp_samp_ts_{ts}"\n')
+            myfile.write(f'output_dir = "{system}_{type_of_sim}"\n')
             myfile.close()